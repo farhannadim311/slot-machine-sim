185f291952a8a80bb89c239557aa17f64513291d
farhannadim311 <farhannadim311@gmail.com>
2024-10-31 17:01:18 -0500

    added object for slot tiles

--- project.js
+++ project.js
@@ -1,6 +1,20 @@
 const prompt = require("prompt-sync")();
 const ROWS = 3;
 const COLS = 3;
+const SYMBOLS_COUNT =
+{
+    "A" : 2,
+    "B" : 4,
+    "C" : 6,
+    "D" : 8
+}
+const SYMBOLS_VALUE = 
+{
+    "A" : 5,
+    "B" : 4,
+    "C" : 3,
+    "D" : 2, 
+}
 
 const GetDeposit = () =>
 {
