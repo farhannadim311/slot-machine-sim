3422ef7d943cc3ba86bce8426852dd89e7ba5d73
farhannadim311 <farhannadim311@gmail.com>
2024-10-31 16:53:03 -0500

    added input functionality

---
+++ project.js
@@ -0,0 +1,56 @@
+const prompt = require("prompt-sync")();
+const ROWS = 3;
+const COLS = 3;
+
+const GetDeposit = () =>
+{
+    while (true)
+    {
+        const deposit = prompt("Enter the amount of money that you want to play with: ");
+        const NumDeposit = parseFloat(deposit);
+        if (isNaN(NumDeposit) || NumDeposit < 0)
+        {
+            console.log("Invalid amount entered. Please try again");
+        }
+        else
+        {
+            return NumDeposit;
+        }
+    }
+};
+const GetLine = () =>
+    {
+        while (true)
+        {
+            const Line = prompt("Enter the number of lines that you want to play with(1-3): ");
+            const Linenumber = parseFloat(Line);
+            if (isNaN(Linenumber) || Linenumber < 0 || Linenumber > 3)
+            {
+                console.log("Invalid line number entered, try again");
+            }
+            else
+            {
+                return Linenumber;
+            }
+        }
+    };
+const Getbet = (balance, lines) =>
+{
+    while (true)
+        {
+            const bet = prompt("Enter the bet per line: ");
+            const BetNumber = parseFloat(bet);
+            if (isNaN(BetNumber) || BetNumber < 0 || BetNumber * lines > balance)
+            {
+                console.log("Invalid bet, try again");
+            }
+            else
+            {
+                return BetNumber;
+            }
+        } 
+};
+let balance = GetDeposit();
+const lines = GetLine();
+const bet = Getbet(balance, lines);
+
